1b5fc67479202f8773d67e694309133973b56de0
goawork <sopiagorgadze@gmail.com>
2025-04-22 04:03:33 +0400

    commit

---
+++ basics of oop/ practice/task013.py
@@ -0,0 +1,33 @@
+
+class Prey:
+    def flee(self):
+        print("this animal is fleeing")
+
+class Predator:
+    def hunt(self):
+        print("this animal is huntinh")
+ 
+class Rabbit(Prey):
+    pass
+ 
+class Hawk(Predator):
+    pass
+ 
+
+class Fish(Prey, Predator):
+    pass
+
+
+rabbit = Rabbit()
+
+rabbit.flee()
+# ======
+hawk = Hawk()
+
+hawk.hunt()
+# ======
+fish = Fish()
+
+fish.flee()
+fish.hunt()
+# ======
